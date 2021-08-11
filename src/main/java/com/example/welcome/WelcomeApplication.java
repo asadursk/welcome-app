3877d59309a49ac2016865d7d8ee523535dac803
Sadurska <aleksandra.sadurska@capgemini.com>
2021-08-11 08:47:07 +0200

    Remove unused configuration instance.

--- src/main/java/com/example/welcome/WelcomeApplication.java
+++ src/main/java/com/example/welcome/WelcomeApplication.java
@@ -1,6 +1,5 @@
 package com.example.welcome;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -8,9 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WelcomeApplication implements CommandLineRunner {
-
-	@Autowired
-	private CustomConfig myConfig;
 
 	@Value("${spring.description}")
 	private String description;
